cc52876d3867ba496719a49d59f32db4e276b65c
Bwananah <ewan.golfier@epfl.ch>
2021-03-08 19:03:02 +0100

    Make it possible to create and see posts in forum

--- app/src/main/java/com/github/epfl/meili/forum/ForumActivity.kt
+++ app/src/main/java/com/github/epfl/meili/forum/ForumActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.github.epfl.meili.R
 import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
@@ -19,8 +21,7 @@ class ForumActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forum)
 
-        val posts = getPostsFromDatabase()
-        showPosts(posts)
+        getPostsFromDatabase()
     }
 
     /** Called when the user taps a post */
@@ -35,33 +36,72 @@ class ForumActivity : AppCompatActivity() {
         startActivity(intent) // starts the instance of NewPostActivity
     }
 
-    /** Gets posts from database */
-    private fun getPostsFromDatabase(): ArrayList<QueryDocumentSnapshot> {
+    /** Gets posts from database and shows them asynchronously*/
+    private fun getPostsFromDatabase() {
         // Access Cloud Firestore
         val db = Firebase.firestore
-
-        // List of posts
-        val posts = arrayListOf<QueryDocumentSnapshot>()
 
         // Get the relevant posts
         // TODO: restrict number of posts retrieved
         db.collection("posts")
             .get()
-            .addOnSuccessListener { result ->
+            .addOnSuccessListener { result -> // If success
                 for (document in result) {
-                    posts.add(document)
+                    // Show post in UI
+                    showPost(document)
+
                     Log.d(TAG, "${document.id} => ${document.data}")
                 }
             }
-            .addOnFailureListener { exception ->
+            .addOnFailureListener { exception -> // If failure
                 Log.w(TAG, "Error getting documents.", exception)
             }
-
-        return posts
     }
 
-    /** Shows the posts in the forum UI */
-    private fun showPosts(posts : ArrayList<QueryDocumentSnapshot>) {
-        // TODO: Create UI for each post in the list
+    /** Shows the post in the forum UI */
+    private fun showPost(post: QueryDocumentSnapshot) {
+        // Create and get linearLayout (box for the information to go in)
+        val linearLayout = addLinearLayout()
+
+        // Add elements that go in the linearLayout
+        addAuthorToPost(post.data.get("username").toString(), linearLayout)
+        addTitleToPost(post.data.get("title").toString(), linearLayout)
     }
+
+    /** Creates a linearLayout for the post's information to be stored in */
+    private fun addLinearLayout() : LinearLayout {
+        // Create layout
+        val linearLayout = LinearLayout(this)
+        linearLayout.orientation = LinearLayout.VERTICAL
+
+        //TODO: make pretty
+
+        // Add it to the parent linearLayout
+        findViewById<LinearLayout>(R.id.forum_layout).addView(linearLayout)
+
+        return linearLayout
+    }
+
+    /** Adds the author to the post UI */
+    private fun addAuthorToPost(author: String, linearLayout: LinearLayout) {
+        // Create TextView
+        val textView = TextView(this)
+        textView.text = author
+
+        //TODO: make pretty
+
+        linearLayout.addView(textView)
+    }
+
+    /** Adds the title to the post UI */
+    private fun addTitleToPost(title: String, linearLayout: LinearLayout) {
+        // Create TextView
+        val textView = TextView(this)
+        textView.text = title
+
+        //TODO: make pretty
+
+        linearLayout.addView(textView)
+    }
+
 }
--- app/src/main/java/com/github/epfl/meili/forum/PostActivity.kt
+++ app/src/main/java/com/github/epfl/meili/forum/PostActivity.kt
@@ -9,5 +9,8 @@ class PostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+
+        // TODO: Show post on creation
     }
+
 }
