91aeaf05430e871ac4dd6fbfec6da56a7c6017b2
Aman Bansal <aman.bansal@epfl.ch>
2021-03-08 12:32:48 +0100

    rename package

--- app/src/androidTest/java/com/github/epfl/meili/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/epfl/meili/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.github.marceltorne.meili
+package com.github.epfl.meili
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
