e8b3888a342d71ff375814ed81762d6859d78fe3
ahmedc3ca <ahmed.ezzo@epfl.ch>
2021-03-08 19:36:49 +0200

    cirrus new key

---
+++ app/src/main/java/com/github/epfl/meili/ChatMessage.kt
@@ -0,0 +1,5 @@
+package com.github.epfl.meili
+
+class ChatMessage(val id: String, val text: String, val fromId: String, val toId: String, val timestamp: Long) {
+    constructor() : this("", "", "", "", -1)
+}
---
+++ app/src/main/java/com/github/epfl/meili/NewMessageActivity.kt
@@ -0,0 +1,73 @@
+package com.github.epfl.meili
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+
+class NewMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_message)
+
+        supportActionBar?.title = "Select User"
+
+
+        fetchUsers()
+    }
+
+    companion object{
+        val USER_KEY = "USERK_KEY"
+    }
+
+    private fun fetchUsers() {
+        val ref = Firebase.database.getReference("/users")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener{
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val adapter = GroupAdapter<GroupieViewHolder>()
+                snapshot.children.forEach{
+                    val user = it.getValue(User::class.java)
+                    if(user != null){
+                        adapter.add(UserItem(user))
+                    }
+                }
+                adapter.setOnItemClickListener { item, view ->
+                    val userItem = item as UserItem
+                    val intent = Intent(view.context, ChatLogActivity::class.java)
+                    intent.putExtra(USER_KEY, userItem.user)
+                    startActivity(intent)
+
+                    finish()
+                }
+                findViewById<RecyclerView>(R.id.recyclerview_newmessage).adapter = adapter
+            }
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+}
+
+class UserItem(val user: User): Item<GroupieViewHolder>() {
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_message
+    }
+
+
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        viewHolder.itemView.findViewById<TextView>(R.id.username_text_view_newmessage).text = user.username
+    }
+
+}
