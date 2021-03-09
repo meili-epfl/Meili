8385aaa653229c0124298fcd383cb5c045d0be06
☆ミ <duanyingxuan@Ys-MacBook-Air.local>
2021-03-09 01:18:58 +0100

    can modify fields

--- app/src/main/java/com/github/epfl/meili/ModifyProfileActivity.kt
+++ app/src/main/java/com/github/epfl/meili/ModifyProfileActivity.kt
@@ -3,11 +3,31 @@ package com.github.epfl.meili
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class ModifyProfileActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modify_profile)
+    }
+
+    fun sendUpdates(view: View) {
+        val newName = findViewById<EditText>(R.id.etName)
+        val newPhone = findViewById<EditText>(R.id.etPhone).text.toString()
+        val newDescription = findViewById<EditText>(R.id.etDescription).text.toString()
+        val intent = Intent(this, ProfileActivity::class.java)
+        val extras = Bundle()
+        extras.putString("EXTRA_USERNAME", newName.text.toString())
+        extras.putString("EXTRA_PHONE", newPhone)
+        extras.putString("EXTRAS_DES", newDescription)
+        intent.putExtras(extras)
+        startActivity(intent)
+    }
+
+    fun cancelGoBack(){
+        //bug when cancel if fields are filled
+        val intent = Intent(this, ProfileActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/github/epfl/meili/ProfileActivity.kt
+++ app/src/main/java/com/github/epfl/meili/ProfileActivity.kt
@@ -3,6 +3,7 @@ package com.github.epfl.meili
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -11,6 +12,24 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+
+        val extras = intent.extras
+        if(extras!=null) {
+            val username_string = extras!!.getString("EXTRA_USERNAME")
+            val phone_string = extras!!.getString("EXTRA_PHONE")
+            val des_string = extras!!.getString("EXTRAS_DES")
+
+            val tvName = findViewById<TextView>(R.id.tvName).apply {
+                text = username_string
+            }
+
+            val tvPhone = findViewById<TextView>(R.id.tvPhone).apply {
+                text = phone_string
+            }
+            val tvDescription = findViewById<TextView>(R.id.tvDescription)
+            tvDescription.setText(des_string)
+        }
     }
 
     fun modProfile(view: View) {
