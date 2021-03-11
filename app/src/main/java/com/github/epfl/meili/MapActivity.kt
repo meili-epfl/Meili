358da018662cf695867ee2ce700bc97d6380b572
Aman Bansal <aman.bansal@epfl.ch>
2021-03-11 21:46:11 +0100

    permission tests work from AS but not with gradlew (terminal)

--- app/src/main/java/com/github/epfl/meili/MapActivity.kt
+++ app/src/main/java/com/github/epfl/meili/MapActivity.kt
@@ -1,6 +1,7 @@
 package com.github.epfl.meili
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
@@ -15,7 +16,6 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.PointOfInterest
@@ -25,21 +25,16 @@ import com.google.android.libraries.places.api.net.PlacesClient
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnPoiClickListener {
     companion object {
-        private val CAMERA_POSITION_KEY = "camera_position"
-        private val LOCATION_KEY = "location"
-        private val TAG = MapActivity::class.java.simpleName
+        private val TAG = MapActivity::class.java.name
         private val DEFAULT_ZOOM = 15
-        private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: Int = 1
+        private val REQUEST_CODE: Int = 1
     }
 
     // API entry points
     private lateinit var placesClient: PlacesClient
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
-    private var locationPermission = false
-
     private lateinit var map: GoogleMap
-    private var cameraPosition: CameraPosition? = null
 
     private val defaultLocation = LatLng(48.864716, 2.349014) // Paris
     private var location: Location? = null
@@ -58,23 +53,15 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnPoiClic
         this.fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         // Initialize map
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment?
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
+    }
 
-        // Retrieve previously saved state
-        if (savedInstanceState != null) {
-            this.cameraPosition = savedInstanceState.getParcelable(CAMERA_POSITION_KEY)
-            this.location = savedInstanceState.getParcelable(LOCATION_KEY)
-        }
-
-        if (ContextCompat.checkSelfPermission(
-                        this.applicationContext,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            locationPermission = true
-        }
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this.applicationContext,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     override fun onRequestPermissionsResult(
@@ -82,15 +69,12 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnPoiClic
         permissions: Array<String>,
         grantResults: IntArray
     ) {
-        this.locationPermission = false
-
         // Check whether permission was granted
-        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION && grantResults.isNotEmpty() &&
+        if (requestCode == REQUEST_CODE && grantResults.isNotEmpty() &&
             grantResults[0] == PackageManager.PERMISSION_GRANTED
         )
-            this.locationPermission = true
-
-        updateMapUI()
+            updateMapUI()
+        getDeviceLocation()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -101,6 +85,9 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnPoiClic
                 this, R.raw.map_style
             )
         )
+        if (!isPermissionGranted()) {
+            getLocationPermission()
+        }
 
         updateMapUI()
         getDeviceLocation()
@@ -114,70 +101,55 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnPoiClic
         ).show()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        map.let { map ->
-            outState.putParcelable(CAMERA_POSITION_KEY, map.cameraPosition)
-            outState.putParcelable(LOCATION_KEY, this.location)
-        }
-        super.onSaveInstanceState(outState)
-    }
-
     private fun getLocationPermission() {
-        if (!locationPermission) {
-            ActivityCompat.requestPermissions(
-                this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
-            )
-        }
+        assert(!isPermissionGranted())
+        ActivityCompat.requestPermissions(
+            this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_CODE
+        )
     }
 
+    @SuppressLint("MissingPermission")
     private fun updateMapUI() {
-        try {
-            if (this.locationPermission) {
-                this.map.isMyLocationEnabled = true
-                this.map.uiSettings?.isMyLocationButtonEnabled = true
-                // Need to figure out how to center camera on location when app is opened instead of needing user to press on location button
-            } else {
-                this.map.isMyLocationEnabled = false
-                this.map.uiSettings?.isMyLocationButtonEnabled = false
-                this.location = null
-                getLocationPermission()
-            }
-        } catch (e: SecurityException) {
-            Log.e("Exception: %s", e.message, e)
+        if (isPermissionGranted()) {
+            map.isMyLocationEnabled = true
+            this.map.uiSettings?.isMyLocationButtonEnabled = true
+            // TODO: center camera on location when app is opened instead of needing user to press on location button
+        } else {
+            map.isMyLocationEnabled = false
+            map.uiSettings?.isMyLocationButtonEnabled = false
+            location = null
+            Toast.makeText(this, "Cannot show location without permission", Toast.LENGTH_LONG)
         }
     }
 
+    @SuppressLint("MissingPermission")
     private fun getDeviceLocation() {
-        try {
-            if (this.locationPermission) {
-                val locationResult = this.fusedLocationProviderClient.lastLocation
-                locationResult.addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        this.location = task.result
-                        if (this.location != null) {
-                            this.map.moveCamera(
-                                CameraUpdateFactory.newLatLngZoom(
-                                    LatLng(
-                                        this.location!!.latitude,
-                                        this.location!!.longitude
-                                    ), DEFAULT_ZOOM.toFloat()
-                                )
+        if (isPermissionGranted()) {
+            val locationResult = this.fusedLocationProviderClient.lastLocation
+            locationResult.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    location = task.result
+                    if (location != null) {
+                        map.moveCamera(
+                            CameraUpdateFactory.newLatLngZoom(
+                                LatLng(
+                                    location!!.latitude,
+                                    location!!.longitude
+                                ), DEFAULT_ZOOM.toFloat()
                             )
-                        }
-                    } else {
-                        Log.d(TAG, "Current location is null. Using defaults.")
-                        Log.e(TAG, "Exception: %s", task.exception)
-                        this.map.moveCamera(
-                            CameraUpdateFactory
-                                .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat())
                         )
-                        this.map.uiSettings?.isMyLocationButtonEnabled = false
                     }
+                } else {
+                    Log.d(TAG, "Current location is null. Using defaults.")
+                    Log.e(TAG, "Exception: %s", task.exception)
+                    this.map.moveCamera(
+                        CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat())
+                    )
+                    this.map.uiSettings?.isMyLocationButtonEnabled = false
                 }
             }
-        } catch (e: SecurityException) {
-            Log.e("Exception: %s", e.message, e)
         }
     }
 }
