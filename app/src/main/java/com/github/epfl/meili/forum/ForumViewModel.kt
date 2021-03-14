696b5e50ef2e30a0a4b93143cc6b895be7778ae2
Bwananah <ewan.golfier@epfl.ch>
2021-03-14 16:56:24 +0100

    Add Forum ViewModel

--- app/src/main/java/com/github/epfl/meili/forum/ForumViewModel.kt
+++ app/src/main/java/com/github/epfl/meili/forum/ForumViewModel.kt
@@ -2,28 +2,18 @@ package com.github.epfl.meili.forum
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import java.util.*
 
-object ForumViewModel : ViewModelWithPostService(), Observer {
+class ForumViewModel : ViewModel() {
 
     private val _posts = MutableLiveData<List<Post>>() // private list prevents exposing mutability
     val posts: LiveData<List<Post>> = _posts // public list to expose the posts to the View
 
     init {
-        postService.addObserver(this) // Observe changes from the service
-    }
-
-    /** Called when notified to update posts */
-    override fun update(o: Observable?, arg: Any?) {
-        syncPosts()
-    }
-
-    /** Synchronize posts */
-    private fun syncPosts() {
         viewModelScope.launch { // Asynchronous block for viewModels
-            _posts.value = postService.getPosts()
+            _posts.value = PostService.getPosts()
         }
     }
 }
