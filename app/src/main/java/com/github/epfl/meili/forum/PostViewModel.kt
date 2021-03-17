31a039322b4954c937ed7da541f9733ef185775c
Bwananah <ewan.golfier@epfl.ch>
2021-03-17 13:54:42 +0100

    Refactor posts

--- app/src/main/java/com/github/epfl/meili/forum/PostViewModel.kt
+++ app/src/main/java/com/github/epfl/meili/forum/PostViewModel.kt
@@ -2,35 +2,18 @@ package com.github.epfl.meili.forum
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import java.util.*
 
-object PostViewModel : ViewModelWithPostService(), Observer {
+class PostViewModel(post_id: String) : ViewModel() {
 
     private val _post = MutableLiveData<Post?>() // makes it easier to manage
     val post: LiveData<Post?> = _post // public post to expose safely to the View (not mutable)
-    var post_id: String = ""
 
     init {
-        postService.addObserver(this)
-    }
-
-    /** Called when notified by observable */
-    override fun update(o: Observable?, arg: Any?) {
-        syncPost()
-    }
-
-    /** Synchronize post */
-    private fun syncPost() {
         viewModelScope.launch { // Asynchronous block for viewModels
-            _post.value = postService.getPostFromId(post_id)
+            _post.value = PostService.getPostFromId(post_id)  // can be null
         }
-    }
-
-    /** Change id of post to be seen */
-    fun setID(id: String) {
-        post_id = id
-        syncPost()
     }
 }
