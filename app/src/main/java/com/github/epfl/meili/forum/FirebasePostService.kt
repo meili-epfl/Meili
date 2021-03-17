92e8bcb8d86c07aa6b46e485517166ac747cdd7d
Bwananah <ewan.golfier@epfl.ch>
2021-03-17 18:50:13 +0100

    Refactor code to make make mocking possible

---
+++ app/src/main/java/com/github/epfl/meili/forum/FirebasePostService.kt
@@ -0,0 +1,61 @@
+package com.github.epfl.meili.forum
+
+import android.util.Log
+import com.github.epfl.meili.forum.Post.Companion.toPost
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
+
+object FirebasePostService : PostService {
+
+    private const val TAG = "FirebasePostService"
+
+    /** Get Post data from its id */
+    override suspend fun getPostFromId(id: String?): Post? { // suspend makes function asynchronous
+        if (id == null) {
+            return null
+        }
+
+        val db = Firebase.firestore
+        return try {
+            db.collection("posts")
+                .document(id)
+                .get()
+                .await() // wait asynchronously for data to arrive
+                .toPost() // Convert to Post instance
+        } catch (e: Exception) {
+            Log.e(TAG, "Error getting Post from database")
+            null // Return null if error occurs
+        }
+    }
+
+    /** Get multiple posts from Database */
+    override suspend fun getPosts(): List<Post> {
+        val db = Firebase.firestore
+        return try {
+            db.collection("posts")
+                .get()
+                .await() // wait asynchronously for data to arrive
+                .documents.mapNotNull { it.toPost() } // Convert all conforming Posts to Post and add to list
+        } catch (e: Exception) {
+            Log.e(TAG, "Error getting multiple posts from database")
+            emptyList<Post>() // Return empty list if error occurs
+        }
+    }
+
+    /** Add new post to Database */
+    override fun addPost(author: String, title: String, text: String) {
+        val db = Firebase.firestore
+
+        // Create post document (ID created by database)
+        val postDocument = hashMapOf(
+            "username" to author,
+            "title" to title,
+            "text" to text
+        )
+
+        // Add a new document with a generated ID
+        db.collection("posts").add(postDocument)
+    }
+
+}
--- app/src/main/java/com/github/epfl/meili/forum/NewPostViewModel.kt
+++ app/src/main/java/com/github/epfl/meili/forum/NewPostViewModel.kt
@@ -1,22 +1,20 @@
 package com.github.epfl.meili.forum
 
 import android.util.Log
-import androidx.lifecycle.ViewModel
 import com.github.epfl.meili.home.AuthenticationService
 
-class NewPostViewModel : ViewModel() {
+class NewPostViewModel : ViewModelWithPostService() {
 
     private val TAG = "NewPostViewModel"
 
-    // Create new post in database for this particular user
+    /** Create new post in database for this particular user */
     fun createNewPost(title: String, text: String) {
         val currentUser = AuthenticationService.getCurrentUser()
 
         if (currentUser != null) {
-            PostService.addPost(currentUser.displayName, title, text)
+            postService.addPost(currentUser.displayName, title, text)
         } else {
             Log.e(TAG, "Error: non-logged in user is trying to create post")
         }
     }
-
 }
