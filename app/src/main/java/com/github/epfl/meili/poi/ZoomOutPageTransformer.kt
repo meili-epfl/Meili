1418b34de96185ce61123d811d929826f9a02aac
Thomas Berkane <thomas.berkane@epfl.ch>
2021-03-18 17:22:19 +0100

    Refactor method to make it shorterer.

--- app/src/main/java/com/github/epfl/meili/poi/ZoomOutPageTransformer.kt
+++ app/src/main/java/com/github/epfl/meili/poi/ZoomOutPageTransformer.kt
@@ -25,8 +25,7 @@ class ZoomOutPageTransformer : ViewPager2.PageTransformer {
 
                     scaleX = scaleFactor
                     scaleY = scaleFactor
-                    alpha = (MIN_ALPHA +
-                            (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
+                    alpha = (MIN_ALPHA + (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
                 }
                 else ->
                     alpha = 0f
