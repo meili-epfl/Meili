0c1ab9d1718dffc09cfcb9e4b7ca26e35c64f1de
Thomas Berkane <thomas.berkane@epfl.ch>
2021-03-18 17:15:56 +0100

    Refactor poi fragments.

--- app/src/main/java/com/github/epfl/meili/poi/PoiFragment.kt
+++ app/src/main/java/com/github/epfl/meili/poi/PoiFragment.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
-import com.github.epfl.meili.R
 
-class ChatFragment : Fragment() {
+class PoiFragment(@LayoutRes val layoutId: Int) : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.chat_activity_placeholder, container, false)
+    ): View = inflater.inflate(layoutId, container, false)
 }
