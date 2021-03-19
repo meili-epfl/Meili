a0562312f04497e47b8ff1b7a0cbea81f073da7a
ahmedc3ca <ahmed.ezzo@epfl.ch>
2021-03-19 13:17:46 +0100

    better UserUnit Tests

--- app/src/androidTest/java/com/github/epfl/meili/models/UserUnitTest.kt
+++ app/src/androidTest/java/com/github/epfl/meili/models/UserUnitTest.kt
@@ -25,18 +25,46 @@ class UserUnitTest {
         assertThat(user.component2(), `is`(TEST_USERNAME))
 
 
+        user.copy(TEST_UID, TEST_USERNAME)
+        user.writeToParcel(Parcel.obtain(), 0)
+    }
+
+    @Test
+    fun describeContentDefaultIsZero(){
+        var user = User(TEST_UID, TEST_USERNAME)
+
+
         assertThat(user.describeContents(), `is`(0))
-        assertThat(user.hashCode(), `is`(1110174274))
+    }
+
+    @Test
+    fun hashCodeIsConsistent(){
+        var user = User(TEST_UID, TEST_USERNAME)
+
+        var otherUser = User(TEST_UID, TEST_USERNAME)
+
+
+        assertThat(user.hashCode(), `is`(otherUser.hashCode()))
+    }
+
+    @Test
+    fun toStringShowsFields(){
+        var user = User(TEST_UID, TEST_USERNAME)
+
+
         assertThat(user.toString(), `is`("User(uid=test_uid, username=moderator)"))
+    }
+
+    @Test
+    fun equalsBehavesAsExpected(){
+        var user = User(TEST_UID, TEST_USERNAME)
 
         var otherSameUser = User(TEST_UID, TEST_USERNAME)
         assertThat(user.equals(otherSameUser), `is`(true))
         var otherUser = User("", TEST_USERNAME)
         assertThat(user.equals(otherUser), `is`(false))
-
-        user.copy(TEST_UID, TEST_USERNAME)
-        user.writeToParcel(Parcel.obtain(), 0)
+        var nullUser = null
+        assertThat(user.equals(nullUser), `is`(false))
     }
-
 
 }
