f8ab6cec82ad6de4fe94a62fb40d519888449ddb
Bwananah <ewan.golfier@epfl.ch>
2021-03-22 14:32:14 +0100

    Start camera activity

--- app/src/main/java/com/github/epfl/meili/MainActivity.kt
+++ app/src/main/java/com/github/epfl/meili/MainActivity.kt
@@ -6,8 +6,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.github.epfl.meili.home.GoogleSignInActivity
 import com.github.epfl.meili.map.MapActivity
-import com.github.epfl.meili.poi.PoiActivity
-import com.github.epfl.meili.registerlogin.RegisterActivity
+import com.github.epfl.meili.photo.CameraActivity
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.PointOfInterest
 
@@ -28,13 +27,19 @@ class MainActivity : AppCompatActivity() {
             R.id.launchMapView -> {
                 Intent(this, MapActivity::class.java)
             }
+            R.id.launchCameraView -> {
+                Intent(this, CameraActivity::class.java)
+            }
             else -> {
                 Intent(this, MainActivity::class.java)
             }
         }
 
-        if(view.id == R.id.launchChatView){
-            intent.putExtra("POI_KEY", PointOfInterest(LatLng(100.0,100.0),"tour-eiffel1", "tour-eiffel2"))
+        if (view.id == R.id.launchChatView) {
+            intent.putExtra(
+                "POI_KEY",
+                PointOfInterest(LatLng(100.0, 100.0), "tour-eiffel1", "tour-eiffel2")
+            )
         }
         startActivity(intent)
     }
