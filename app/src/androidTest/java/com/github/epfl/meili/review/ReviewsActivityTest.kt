6c9a781da67a2a71c5ce4f4eacfc0f9419f13254
Aman Bansal <aman.bansal@epfl.ch>
2021-03-31 10:33:31 +0200

    add review editing test

--- app/src/androidTest/java/com/github/epfl/meili/review/ReviewsActivityTest.kt
+++ app/src/androidTest/java/com/github/epfl/meili/review/ReviewsActivityTest.kt
@@ -2,6 +2,7 @@ package com.github.epfl.meili.review
 
 import android.content.Intent
 import android.view.View
+import android.widget.EditText
 import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -20,7 +21,6 @@ import com.github.epfl.meili.R
 import com.github.epfl.meili.home.Auth
 import com.github.epfl.meili.models.Review
 import com.google.firebase.firestore.*
-import org.hamcrest.CoreMatchers.anything
 import org.hamcrest.CoreMatchers.not
 import org.hamcrest.Description
 import org.hamcrest.Matcher
@@ -31,7 +31,6 @@ import org.junit.runner.RunWith
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito
 import org.mockito.Mockito.`when`
-import java.lang.Thread.sleep
 
 
 @Suppress("UNCHECKED_CAST")
@@ -49,8 +48,12 @@ class ReviewsActivityTest {
         private const val AVERAGE_FORMAT = "%.2f"
 
         private const val NUM_REVIEWS_BEFORE_ADDITION = 10
+
         private const val ADDED_REVIEW_RATING = 0.5f
         private const val TEST_ADDED_TITLE = "Desert too sandy"
+
+        private const val EDITED_REVIEW_RATING = 5f
+        private const val TEST_EDITED_TITLE = "Looks good to me"
     }
 
     private val mockFirestore: FirebaseFirestore = Mockito.mock(FirebaseFirestore::class.java)
@@ -60,12 +63,14 @@ class ReviewsActivityTest {
 
     private val mockSnapshotBeforeAddition: QuerySnapshot = Mockito.mock(QuerySnapshot::class.java)
     private val mockSnapshotAfterAddition: QuerySnapshot = Mockito.mock(QuerySnapshot::class.java)
+    private val mockSnapshotAfterEdition: QuerySnapshot = Mockito.mock(QuerySnapshot::class.java)
 
     private val mockAuthenticationService = MockAuthenticationService()
     private lateinit var service: FirestoreReviewService
 
     private var testAverageRatingBeforeAddition: Float = 0f
     private var testAverageRatingAfterAddition: Float = 0f
+    private var testAverageRatingAfterEdition: Float = 0f
 
     init {
         setupMocks()
@@ -84,23 +89,29 @@ class ReviewsActivityTest {
         var mockDocumentList = beforeAdditionList()
         `when`(mockSnapshotBeforeAddition.documents).thenReturn(mockDocumentList)
 
-        mockDocumentList = ArrayList(mockDocumentList)
-        mockDocumentList.add(addedReviewDocumentSnapshot())
-        `when`(mockSnapshotAfterAddition.documents).thenReturn(mockDocumentList)
+        val mockDocumentListAfterAddition = ArrayList(mockDocumentList)
+        mockDocumentListAfterAddition.add(addedReviewDocumentSnapshot())
+        `when`(mockSnapshotAfterAddition.documents).thenReturn(mockDocumentListAfterAddition)
+
+        val mockDocumentListAfterEdition = ArrayList(mockDocumentList)
+        mockDocumentListAfterEdition.add(editedReviewDocumentSnapshot())
+        `when`(mockSnapshotAfterEdition.documents).thenReturn(mockDocumentListAfterEdition)
 
         // Inject dependencies
         FirestoreReviewService.databaseProvider = { mockFirestore }
         Auth.authService = mockAuthenticationService
     }
 
+    private fun editedReviewDocumentSnapshot(): DocumentSnapshot {
+        testAverageRatingAfterEdition = (NUM_REVIEWS_BEFORE_ADDITION * testAverageRatingBeforeAddition + EDITED_REVIEW_RATING) /
+                (NUM_REVIEWS_BEFORE_ADDITION + 1)
+        return getMockDocumentSnapshot(TEST_UID, Review(EDITED_REVIEW_RATING, TEST_EDITED_TITLE, TEST_SUMMARY))
+    }
+
     private fun addedReviewDocumentSnapshot(): DocumentSnapshot {
-        val review = Review(ADDED_REVIEW_RATING, TEST_ADDED_TITLE, TEST_SUMMARY)
-        val mockDocumentSnapshot: DocumentSnapshot = Mockito.mock(DocumentSnapshot::class.java)
-        `when`(mockDocumentSnapshot.id).thenReturn(TEST_UID)
-        `when`(mockDocumentSnapshot.toObject(Review::class.java)).thenReturn(review)
         testAverageRatingAfterAddition = (NUM_REVIEWS_BEFORE_ADDITION * testAverageRatingBeforeAddition + ADDED_REVIEW_RATING) /
-                                            (NUM_REVIEWS_BEFORE_ADDITION + 1)
-        return mockDocumentSnapshot
+                (NUM_REVIEWS_BEFORE_ADDITION + 1)
+        return getMockDocumentSnapshot(TEST_UID, Review(ADDED_REVIEW_RATING, TEST_ADDED_TITLE, TEST_SUMMARY))
     }
 
     private fun beforeAdditionList(): MutableList<DocumentSnapshot> {
@@ -109,17 +120,19 @@ class ReviewsActivityTest {
 
         for (i in 1..NUM_REVIEWS_BEFORE_ADDITION) {
             val review = Review(i.toFloat() / 2, TEST_TITLE, TEST_SUMMARY)
-
             reviews[i.toString()] = review
-
-            val mockDocumentSnapshot: DocumentSnapshot = Mockito.mock(DocumentSnapshot::class.java)
-            `when`(mockDocumentSnapshot.id).thenReturn(i.toString())
-            `when`(mockDocumentSnapshot.toObject(Review::class.java)).thenReturn(review)
-            documentList.add(mockDocumentSnapshot)
+            documentList.add(getMockDocumentSnapshot(i.toString(), review))
         }
 
         testAverageRatingBeforeAddition = Review.averageRating(reviews)
         return documentList
+    }
+
+    private fun getMockDocumentSnapshot(id: String, review: Review): DocumentSnapshot {
+        val mockDocumentSnapshot: DocumentSnapshot = Mockito.mock(DocumentSnapshot::class.java)
+        `when`(mockDocumentSnapshot.id).thenReturn(id)
+        `when`(mockDocumentSnapshot.toObject(Review::class.java)).thenReturn(review)
+        return mockDocumentSnapshot
     }
 
     private val intent = Intent(getInstrumentation().targetContext.applicationContext, ReviewsActivity::class.java)
@@ -136,7 +149,7 @@ class ReviewsActivityTest {
         onView(withId(R.id.list_reviews)).check(matches(isDisplayed()))
         onView(withId(R.id.edit_review)).check(matches(not(isDisplayed())))
 
-        onView(withId(R.id.average_rating)).check(matches(textViewWithText(AVERAGE_FORMAT.format(testAverageRatingBeforeAddition))))
+        onView(withId(R.id.average_rating)).check(matches(textViewContainsText(AVERAGE_FORMAT.format(testAverageRatingBeforeAddition))))
 
         onView(withId(R.id.fab_add_edit_review)).check(matches(isNotEnabled()))
         onView(withId(R.id.fab_add_edit_review)).check(matches(not(isDisplayed())))
@@ -150,7 +163,7 @@ class ReviewsActivityTest {
         onView(withId(R.id.list_reviews)).check(matches(isDisplayed()))
         onView(withId(R.id.edit_review)).check(matches(not(isDisplayed())))
 
-        onView(withId(R.id.average_rating)).check(matches(textViewWithText(AVERAGE_FORMAT.format(testAverageRatingBeforeAddition))))
+        onView(withId(R.id.average_rating)).check(matches(textViewContainsText(AVERAGE_FORMAT.format(testAverageRatingBeforeAddition))))
 
         onView(withId(R.id.fab_add_edit_review)).check(matches(isEnabled()))
         onView(withId(R.id.fab_add_edit_review)).check(matches(isDisplayed()))
@@ -187,7 +200,7 @@ class ReviewsActivityTest {
 
         onView(withId(R.id.submit_review)).perform(click())
 
-        //send updated reviews map to review service
+        // send reviews map with added review to review service
         service.onEvent(mockSnapshotAfterAddition, null)
 
         onView(withId(R.id.list_reviews)).check(matches(isDisplayed()))
@@ -198,22 +211,85 @@ class ReviewsActivityTest {
                 .perform(RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(hasDescendant(withText(TEST_UID))))
 
         onView(withText(TEST_UID)).check(matches(isDisplayed()))
-        onView(textViewWithText(TEST_ADDED_TITLE)).check(matches(isDisplayed()))
+        onView(textViewContainsText(TEST_ADDED_TITLE)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.average_rating)).check(matches(textViewWithText(AVERAGE_FORMAT.format(testAverageRatingAfterAddition))))
+        onView(withId(R.id.average_rating)).check(matches(textViewContainsText(AVERAGE_FORMAT.format(testAverageRatingAfterAddition))))
     }
 
-    private fun textViewWithText(content: String): Matcher<View?> {
+    @Test
+    fun signedInEditReviewTest() {
+        mockAuthenticationService.signInIntent()
+        service.onEvent(mockSnapshotAfterAddition, null) // mock user has existing review
+
+        onView(withId(R.id.fab_add_edit_review)).perform(click())
+
+        // old review is displayed
+        onView(withId(R.id.edit_title)).check(matches(editTextContainsText(TEST_ADDED_TITLE)))
+        onView(withId(R.id.edit_summary)).check(matches(editTextContainsText(TEST_SUMMARY)))
+        onView(withId(R.id.rating_bar)).check(matches(ratingBarHasRating(ADDED_REVIEW_RATING)))
+
+        // edit review
+        onView(withId(R.id.rating_bar)).perform(setRating(EDITED_REVIEW_RATING))
+        onView(withId(R.id.edit_title)).perform(clearText(), typeText(TEST_EDITED_TITLE), closeSoftKeyboard())
+        onView(withId(R.id.submit_review)).perform(click())
+
+        // send reviews map with edited review to review service
+        service.onEvent(mockSnapshotAfterEdition, null)
+
+        onView(withId(R.id.list_reviews)).check(matches(isDisplayed()))
+        onView(withId(R.id.edit_review)).check(matches(not(isDisplayed())))
+
+        onView(withId(R.id.recycler_view))
+            .check(matches(isDisplayed()))
+            .perform(RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(hasDescendant(withText(TEST_UID))))
+
+        onView(withText(TEST_UID)).check(matches(isDisplayed()))
+        onView(textViewContainsText(TEST_EDITED_TITLE)).check(matches(isDisplayed()))
+
+        onView(withText(TEST_ADDED_TITLE)).check(doesNotExist())
+
+        onView(withId(R.id.average_rating)).check(matches(textViewContainsText(AVERAGE_FORMAT.format(testAverageRatingAfterEdition))))
+    }
+
+    private fun ratingBarHasRating(rating: Float): Matcher<View?> {
+        return object : TypeSafeMatcher<View?>() {
+            override fun describeTo(description: Description) {
+                description.appendText("A RatingBar with the value: $rating")
+            }
+
+            override fun matchesSafely(item: View?): Boolean {
+                when (item) {
+                    is RatingBar -> return item.rating == rating
+                }
+                return false
+            }
+        }
+    }
+
+    private fun textViewContainsText(content: String): Matcher<View?> {
         return object : TypeSafeMatcher<View?>() {
             override fun describeTo(description: Description) {
                 description.appendText("A TextView with the text: $content")
             }
 
-            override fun matchesSafely(view: View?): Boolean {
-                when (view) {
-                    is TextView -> {
-                        return view.text == content
-                    }
+            override fun matchesSafely(item: View?): Boolean {
+                when (item) {
+                    is TextView -> return item.text == content
+                }
+                return false
+            }
+        }
+    }
+
+    private fun editTextContainsText(content: String): Matcher<View?> {
+        return object : TypeSafeMatcher<View?>() {
+            override fun describeTo(description: Description) {
+                description.appendText("An EditText with the text: $content")
+            }
+
+            override fun matchesSafely(item: View?): Boolean {
+                when (item) {
+                    is EditText -> return item.text.toString() == content
                 }
                 return false
             }
