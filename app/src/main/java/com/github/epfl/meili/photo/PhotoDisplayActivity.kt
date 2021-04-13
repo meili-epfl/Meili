67d31b271c6e7c5ccd8387fc6673edd02cb036ef
Ewan Golfier <ewan.golfier@epfl.ch>
2021-04-13 15:22:26 +0200

    Implement cropping mechanism

--- app/src/main/java/com/github/epfl/meili/photo/PhotoDisplayActivity.kt
+++ app/src/main/java/com/github/epfl/meili/photo/PhotoDisplayActivity.kt
@@ -2,18 +2,32 @@ package com.github.epfl.meili.photo
 
 import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.epfl.meili.R
+import com.theartofdev.edmodo.cropper.CropImageView
 
 
 class PhotoDisplayActivity : AppCompatActivity() {
+    private lateinit var uri: Uri
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_display)
-        val imageView = findViewById<ImageView>(R.id.image_display)
 
-        val uri = intent.getParcelableExtra<Uri>(CameraActivity.URI_KEY)
+        // Setup callbacks
+        val cropButton = findViewById<ImageButton>(R.id.image_crop_button)
+        cropButton.setOnClickListener { cropMode() }
+
+        val imageView = findViewById<ImageView>(R.id.image_display)
+        uri = intent.getParcelableExtra<Uri>(CameraActivity.URI_KEY)!!
         imageView.setImageURI(uri)
+    }
+
+    /** Callback function for crop button */
+    private fun cropMode() {
+        val cropImageView = findViewById<CropImageView>(R.id.crop_image)
+        cropImageView.setImageUriAsync(uri)
     }
 }
