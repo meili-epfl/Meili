74ff26bf90403b6ece9ceb39c782ba16ea7f0f0f
Ewan Golfier <ewan.golfier@epfl.ch>
2021-04-13 16:57:02 +0200

    Add crop functionality

--- app/src/main/java/com/github/epfl/meili/photo/PhotoDisplayActivity.kt
+++ app/src/main/java/com/github/epfl/meili/photo/PhotoDisplayActivity.kt
@@ -2,6 +2,8 @@ package com.github.epfl.meili.photo
 
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -12,22 +14,66 @@ import com.theartofdev.edmodo.cropper.CropImageView
 class PhotoDisplayActivity : AppCompatActivity() {
     private lateinit var uri: Uri
 
+    private lateinit var imageView: ImageView
+    private lateinit var cropImageView: CropImageView
+    private lateinit var cropModeButton: ImageButton
+    private lateinit var cancelButton: Button
+    private lateinit var cropButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_display)
 
-        // Setup callbacks
-        val cropButton = findViewById<ImageButton>(R.id.image_crop_button)
-        cropButton.setOnClickListener { cropMode() }
+        // Initialize views
+        imageView = findViewById<ImageView>(R.id.image_display)
+        cropImageView = findViewById<CropImageView>(R.id.crop_image)
+        cropImageView.setOnCropImageCompleteListener { view, result ->
+            imageView.setImageDrawable(null) // required hack to update image using same uri
+            imageView.setImageURI(uri) // Set imageView to new cropped image
+            resetVisibilities() // Go back to main screen
+        }
 
-        val imageView = findViewById<ImageView>(R.id.image_display)
+        cropModeButton = findViewById<ImageButton>(R.id.image_crop_mode_button)
+        cancelButton = findViewById<Button>(R.id.image_cancel_button)
+        cropButton = findViewById<Button>(R.id.image_crop_button)
+
+        // Setup callbacks
+        cropModeButton.setOnClickListener { cropMode() }
+        cancelButton.setOnClickListener { resetVisibilities() }
+        cropButton.setOnClickListener { cropImage() }
+
+        resetVisibilities()
+
+        // Set imageView to given image from camera activity
         uri = intent.getParcelableExtra<Uri>(CameraActivity.URI_KEY)!!
         imageView.setImageURI(uri)
     }
 
-    /** Callback function for crop button */
+    /** Callback function for cancel button */
+    private fun resetVisibilities() {
+        // Set visibility of required views
+        imageView.visibility = View.VISIBLE
+        cropImageView.visibility = View.GONE
+        cropModeButton.visibility = View.VISIBLE
+        cancelButton.visibility = View.GONE
+        cropButton.visibility = View.GONE
+    }
+
+    /** Callback function for crop mode button */
     private fun cropMode() {
-        val cropImageView = findViewById<CropImageView>(R.id.crop_image)
-        cropImageView.setImageUriAsync(uri)
+        // Set visibility of required views
+        imageView.visibility = View.GONE
+        cropImageView.visibility = View.VISIBLE
+        cropModeButton.visibility = View.GONE
+        cancelButton.visibility = View.VISIBLE
+        cropButton.visibility = View.VISIBLE
+
+        cropImageView.setImageUriAsync(uri) // Show image in crop tool
+    }
+
+    /** Callback function for crop button */
+    private fun cropImage() {
+        // Async callback to function defined in onCreate()
+        cropImageView.saveCroppedImageAsync(uri)
     }
 }
