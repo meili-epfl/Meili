72f389675fcc53a5b59349975191b8f2284d7914
Aman Bansal <aman.bansal@epfl.ch>
2021-04-13 22:07:08 +0200

    rm useless code

--- app/src/main/java/com/github/epfl/meili/storage/FirebaseStorageService.kt
+++ app/src/main/java/com/github/epfl/meili/storage/FirebaseStorageService.kt
@@ -5,18 +5,16 @@ import com.google.firebase.storage.FirebaseStorage
 
 object FirebaseStorageService: StorageService {
 
-    private val DEFAULT_DATABASE = { FirebaseStorage.getInstance() }
-
-    var storageProvider: () -> FirebaseStorage = DEFAULT_DATABASE
+    private val firebaseStorage = FirebaseStorage.getInstance()
 
     override fun uploadFile(firebasePath: String, filePath: Uri, onSuccessCallback: () -> Unit, onFailureCallback: () -> Unit) {
-        storageProvider().getReference(firebasePath).putFile(filePath)
+        firebaseStorage.getReference(firebasePath).putFile(filePath)
                 .addOnSuccessListener { _ -> onSuccessCallback() }
                 .addOnFailureListener { _ -> onFailureCallback() }
     }
 
     override fun getDownloadUrl(firebasePath: String, onSuccessListener: (Uri) -> Unit, onFailureListener: (Exception) -> Unit) {
-        storageProvider().getReference(firebasePath).downloadUrl
+        firebaseStorage.getReference(firebasePath).downloadUrl
                 .addOnSuccessListener(onSuccessListener)
                 .addOnFailureListener(onFailureListener)
     }
