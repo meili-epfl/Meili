0e44f7ec27232640cf54f3edaff2ecb0a4f5400f
Thomas Berkane <thomas.berkane@epfl.ch>
2021-04-15 16:16:55 +0200

    Fix import

--- app/src/main/java/com/github/epfl/meili/poi/PoiActivity.kt
+++ app/src/main/java/com/github/epfl/meili/poi/PoiActivity.kt
@@ -7,6 +7,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.github.epfl.meili.R
 import com.github.epfl.meili.map.MapActivity
+import com.google.android.gms.maps.model.PointOfInterest
 
 class PoiActivity : AppCompatActivity() {
     companion object {
