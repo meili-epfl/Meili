c02bd6ad2131e262bf74ed703b66d76962804112
ahmedc3ca <ahmed.ezzo@epfl.ch>
2021-04-16 03:24:03 +0200

    Making code climate almost happy :)

--- app/src/main/java/com/github/epfl/meili/forum/ForumActivity.kt
+++ app/src/main/java/com/github/epfl/meili/forum/ForumActivity.kt
@@ -1,9 +1,7 @@
 package com.github.epfl.meili.forum
 
-import OnSwipeTouchListener
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
