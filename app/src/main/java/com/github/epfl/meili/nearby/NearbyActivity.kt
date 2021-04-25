8ceaea0806f39f1b3f4b6db1fc407dbb27d63bca
Aman Bansal <amanbansal3014@protonmail.com>
2021-04-25 16:00:50 +0200

    auto frnd-rqst on conn. and auto disconn.

--- app/src/main/java/com/github/epfl/meili/nearby/NearbyActivity.kt
+++ app/src/main/java/com/github/epfl/meili/nearby/NearbyActivity.kt
@@ -7,23 +7,30 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.github.epfl.meili.BuildConfig
 import com.github.epfl.meili.R
+import com.github.epfl.meili.home.Auth
+import com.github.epfl.meili.models.User
 import com.google.android.gms.nearby.Nearby
 import com.google.android.gms.nearby.connection.*
 
 
 class NearbyActivity : AppCompatActivity() {
-    // TODO supports only a single connection for now
-
     companion object {
         private const val TAG = "NearbyActivity"
         private val STRATEGY = Strategy.P2P_CLUSTER
     }
 
+    private fun addFriend(friendUid: String) {
+        TODO()
+    }
+
     private val payloadCallback = object : PayloadCallback() {
         override fun onPayloadReceived(endpointId: String, payload: Payload) {
-            val bytes: ByteArray = payload.asBytes()!!
-            Toast.makeText(applicationContext, "RECEIVED $bytes", Toast.LENGTH_SHORT).show()
+            val friendUid: String = payload.asBytes()!!.decodeToString()
+            addFriend(friendUid)
+            Toast.makeText(applicationContext, "Friendship successful!", Toast.LENGTH_SHORT).show()
+            connectionsClient.disconnectFromEndpoint(endpointId)
         }
 
         override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {}
@@ -32,8 +39,8 @@ class NearbyActivity : AppCompatActivity() {
     private val connectionLifecycleCallback = object : ConnectionLifecycleCallback() {
         override fun onConnectionInitiated(endpointId: String, info: ConnectionInfo) {
             AlertDialog.Builder(this@NearbyActivity)
-                    .setTitle("Accept connection to ${info.endpointName}")
-                    .setMessage("Confirm the code matches on both devices: ${info.authenticationToken}")
+                    .setTitle("Accept friendship with ${info.endpointName}")
+                    .setMessage("Confirm the code matches on both devices to finalize your friendship: ${info.authenticationToken}")
                     .setPositiveButton("Accept") { _, _ ->
                         connectionsClient.acceptConnection(endpointId, payloadCallback)
                     }
@@ -47,15 +54,17 @@ class NearbyActivity : AppCompatActivity() {
         override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
             when (result.status.statusCode) {
                 ConnectionsStatusCodes.STATUS_OK -> {
-                    Toast.makeText(applicationContext, "CONNECTED!", Toast.LENGTH_SHORT).show()
-                    connectionsClient.stopAdvertising()
-                    connectionsClient.stopDiscovery()
-                    this@NearbyActivity.endpointId = endpointId
+                    Toast.makeText(applicationContext, "Friendship initiated!", Toast.LENGTH_SHORT).show()
+                    val uidPayload = Payload.fromBytes(localUser.uid.toByteArray())
+                    connectionsClient.sendPayload(endpointId, uidPayload)
                 }
                 ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED ->
-                    Toast.makeText(applicationContext, "REJECTED!", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, "Friendship aborted!", Toast.LENGTH_SHORT).show()
                 else -> Log.e(TAG, "CODE: ${result.status.statusCode}")
             }
+
+            connectionsClient.stopAdvertising()
+            connectionsClient.stopDiscovery()
         }
 
         override fun onDisconnected(endpointId: String) {}
@@ -63,9 +72,7 @@ class NearbyActivity : AppCompatActivity() {
 
     private val endpointDiscoveryCallback = object : EndpointDiscoveryCallback() {
         override fun onEndpointFound(endpointId: String, info: DiscoveredEndpointInfo) {
-            connectionsClient.requestConnection(localUserName, endpointId, connectionLifecycleCallback)
-                    .addOnSuccessListener { Toast.makeText(applicationContext, "connection rq sent", Toast.LENGTH_SHORT).show() }
-                    .addOnFailureListener { Toast.makeText(applicationContext, "connection rq failed", Toast.LENGTH_SHORT).show() }
+            connectionsClient.requestConnection(localUser.username, endpointId, connectionLifecycleCallback)
         }
 
         override fun onEndpointLost(endpointId: String) {}
@@ -73,62 +80,40 @@ class NearbyActivity : AppCompatActivity() {
 
     private fun startAdvertising() {
         val advertisingOptions = AdvertisingOptions.Builder().setStrategy(STRATEGY).build()
-        connectionsClient.startAdvertising(localUserName, packageName, connectionLifecycleCallback, advertisingOptions)
-                .addOnSuccessListener { Toast.makeText(applicationContext, "advertising started", Toast.LENGTH_SHORT).show() }
-                .addOnFailureListener { Toast.makeText(applicationContext, "advertising failed", Toast.LENGTH_SHORT).show() }
+        connectionsClient.startAdvertising(localUser.username, packageName, connectionLifecycleCallback, advertisingOptions)
+                .addOnSuccessListener { Toast.makeText(applicationContext, "Looking for your friend!", Toast.LENGTH_SHORT).show() }
+                .addOnFailureListener { Toast.makeText(applicationContext, "Error finding friend", Toast.LENGTH_SHORT).show() }
     }
 
     private fun startDiscovery() {
         val discoveryOptions = DiscoveryOptions.Builder().setStrategy(STRATEGY).build()
         connectionsClient.startDiscovery(packageName, endpointDiscoveryCallback, discoveryOptions)
-                .addOnSuccessListener { Toast.makeText(applicationContext, "discovery started", Toast.LENGTH_SHORT).show() }
-                .addOnFailureListener { Toast.makeText(applicationContext, "discovery failed", Toast.LENGTH_SHORT).show() }
+                .addOnFailureListener { Toast.makeText(applicationContext, "Error finding friend", Toast.LENGTH_SHORT).show() }
     }
 
-    private lateinit var findButton: Button
-    private lateinit var friendButton: Button
-    private lateinit var disconnectButton: Button
-
-    private lateinit var localUserName: String
-
+    private lateinit var findMyFriendButton: Button
+    private lateinit var localUser: User
     private lateinit var connectionsClient: ConnectionsClient
-
-    private var endpointId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nearby)
 
-        localUserName = "Meili User" // TODO
+        if (BuildConfig.DEBUG && Auth.getCurrentUser() == null) {
+            error("Assertion failed")
+        }
 
-        findButton = findViewById(R.id.find)
-        friendButton = findViewById(R.id.friend)
-        disconnectButton = findViewById(R.id.disconnect)
-
+        localUser = Auth.getCurrentUser()!!
+        findMyFriendButton = findViewById(R.id.find_my_friend)
         connectionsClient = Nearby.getConnectionsClient(this)
     }
 
     fun onNearbyButtonClick(view: View) {
         when(view) {
-            findButton -> {
+            findMyFriendButton -> {
                 startAdvertising()
                 startDiscovery()
             }
-            friendButton -> sendFriendRequest()
-            disconnectButton -> disconnect()
-        }
-    }
-
-    private fun sendFriendRequest() {
-        if (endpointId != null) {
-            val bytesPayload = Payload.fromBytes("Friend Request".toByteArray())
-            connectionsClient.sendPayload(endpointId!!, bytesPayload)
-        }
-    }
-
-    private fun disconnect() {
-        if (endpointId != null) {
-            connectionsClient.disconnectFromEndpoint(endpointId!!)
         }
     }
 }
