7154dd2e91f28621daffaadb3c75a56fdfedf2e9
Aman Bansal <amanbansal3014@protonmail.com>
2021-04-28 17:09:28 +0200

    remove unnecessary dependencies

--- app/src/androidTest/java/com/github/epfl/meili/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/epfl/meili/MainActivityTest.kt
@@ -5,8 +5,6 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
-
-import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- app/src/main/java/com/github/epfl/meili/profile/ProfileActivity.kt
+++ app/src/main/java/com/github/epfl/meili/profile/ProfileActivity.kt
@@ -3,6 +3,7 @@ package com.github.epfl.meili.profile
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
@@ -10,13 +11,11 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
 import com.github.epfl.meili.R
 import com.github.epfl.meili.tool.FirestoreTool
 import com.github.epfl.meili.tool.StorageTool
-//import kotlinx.android.synthetic.main.activity_modify_profile.*
-//import kotlinx.android.synthetic.main.activity_profile.*
-//import kotlinx.android.synthetic.main.activity_profile.profilePhoto
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
 import java.io.ByteArrayOutputStream
 
 
@@ -30,7 +29,7 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-            val nimaPhoto =  findViewById<de.hdodenhof.circleimageview.CircleImageView>(R.id.profilePhoto)
+            val nimaPhoto =  findViewById<CircleImageView>(R.id.profilePhoto)
             nimaPhoto.setOnClickListener {
                 val intent = Intent().apply {
                     type = "image/*"
@@ -66,13 +65,10 @@ class ProfileActivity : AppCompatActivity() {
                 .getBitmap(contentResolver, selectedImagePath)
 
             val outputStream = ByteArrayOutputStream()
-            val nimaPhoto = findViewById<de.hdodenhof.circleimageview.CircleImageView>(R.id.profilePhoto) as ImageView
+            val nimaPhoto = findViewById<CircleImageView>(R.id.profilePhoto) as ImageView
             selectedImageBmp.compress(Bitmap.CompressFormat.JPEG, 90, outputStream)
             selectedImageBytes = outputStream.toByteArray()
-            Glide.with(this)
-                .load(selectedImageBytes)
-                .into(nimaPhoto)
-
+            nimaPhoto.setImageBitmap(selectedImageBmp)
             pictureJustChanged = true
         }
     }
@@ -82,14 +78,9 @@ class ProfileActivity : AppCompatActivity() {
         FirestoreTool.getCurrentUser { user ->
             findViewById<EditText>(R.id.tvName).setText(user.username)
             findViewById<EditText>(R.id.tvDescription).setText(user.bio)
-            val nimaPhoto = findViewById<de.hdodenhof.circleimageview.CircleImageView>(R.id.profilePhoto) as ImageView
+            val nimaPhoto = findViewById<CircleImageView>(R.id.profilePhoto) as ImageView
                 if (!pictureJustChanged && user.profilePicturePath != null)
-                    Glide.with(this)
-                        .load(StorageTool.pathToReference(user.profilePicturePath))
-                        //.placeholder(R.drawable.ic_account_circle_black_24dp)
-                        .into(nimaPhoto)
+                    Picasso.get().load(Uri.parse(TODO())).into(nimaPhoto)
             }
         }
-
-
 }
