9fc1b6ee0946fb698bc674141ec314bf64b7f678
Ewan Golfier <ewan.golfier@epfl.ch>
2021-04-29 12:19:05 +0200

    Make tests for RotationGestureDetector

--- app/src/androidTest/java/com/github/epfl/meili/photo/PhotoCropActivityTest.kt
+++ app/src/androidTest/java/com/github/epfl/meili/photo/PhotoCropActivityTest.kt
@@ -1,7 +1,6 @@
 package com.github.epfl.meili.photo
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.UiController
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
