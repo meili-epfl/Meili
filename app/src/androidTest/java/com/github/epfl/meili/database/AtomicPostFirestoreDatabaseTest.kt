4085d804805676a4ee7781b8f63ddc7b50c45913
ahmedc3ca <ahmed.ezzo@epfl.ch>
2021-05-05 19:34:05 +0200

    another try to increase coverage

--- app/src/androidTest/java/com/github/epfl/meili/database/AtomicPostFirestoreDatabaseTest.kt
+++ app/src/androidTest/java/com/github/epfl/meili/database/AtomicPostFirestoreDatabaseTest.kt
@@ -17,6 +17,15 @@ import org.mockito.Mockito
 class AtomicPostFirestoreDatabaseTest {
     private lateinit var db: AtomicPostFirestoreDatabase
 
+    private val postId = "test_id"
+    private val upvoters = arrayListOf<String>("upvoter")
+    private val downvoters = arrayListOf<String>("downvoter")
+
+    private val post = Post("me", "title", "text")
+    private val upvotedPost = Post("me", "title", "text", upvoters)
+    private val downvotedPost = Post("me", "title", "text", downvoters = downvoters)
+
+
     private val mockFirestore: FirebaseFirestore = Mockito.mock(FirebaseFirestore::class.java)
     private val mockCollection: CollectionReference = Mockito.mock(CollectionReference::class.java)
     private val mockDocument: DocumentReference = Mockito.mock(DocumentReference::class.java)
@@ -39,18 +48,86 @@ class AtomicPostFirestoreDatabaseTest {
         // Inject dependencies
         FirestoreDatabase.databaseProvider = { mockFirestore }
         db = AtomicPostFirestoreDatabase("test_path")
-
     }
 
+    private fun getMockDocumentSnapshot(id: String, poi: PointOfInterest): DocumentSnapshot {
+        val mockDocumentSnapshot: DocumentSnapshot = Mockito.mock(DocumentSnapshot::class.java)
+        Mockito.`when`(mockDocumentSnapshot.id).thenReturn(id)
+        Mockito.`when`(mockDocumentSnapshot.toObject(PointOfInterest::class.java)).thenReturn(poi)
+        return mockDocumentSnapshot
+    }
 
     @Test
-    fun upvoteTest() {
-        db.upDownVote("post1", "me", true)
+    fun upvotePostTest() {
+        val mockDocumentReference = Mockito.mock(DocumentReference::class.java)
+
+        Mockito.`when`(mockCollection.document(Mockito.anyString())).then {
+            assertEquals(it.arguments[0], postId)
+            return@then mockDocumentReference
+        }
+        Mockito.`when`(mockDocumentReference.set(Mockito.any())).then {
+            assertEquals(it.arguments[0], upvotedPost)
+            return@then null
+        }
+
+        mockCollection
+        db.upDownVote(postId, upvoters[0], true)
     }
 
     @Test
-    fun downvoteTest() {
-        db.upDownVote("post1", "me", false)
+    fun downvotePostTest() {
+        val mockDocumentReference = Mockito.mock(DocumentReference::class.java)
+
+        Mockito.`when`(mockCollection.document(Mockito.anyString())).then {
+            assertEquals(it.arguments[0], postId)
+            return@then mockDocumentReference
+        }
+        Mockito.`when`(mockDocumentReference.set(Mockito.any())).then {
+            assertEquals(it.arguments[0], downvotedPost)
+            return@then null
+        }
+
+        mockCollection
+        db.upDownVote(postId, downvoters[0], false)
     }
 
+/*    @Test
+    fun onDestroyTest() {
+        Mockito.`when`(mockListenerRegistration.remove()).then {
+            return@then null
+        }
+
+        db.onDestroy()
+    }
+
+    @Test
+    fun onEventErrorTest() {
+        val error = Mockito.mock(FirebaseFirestoreException::class.java)
+        db.onEvent(null, error)
+    }
+
+    @Test
+    fun onEventWithoutSnapshotTest() {
+        db.onEvent(null, null)
+    }
+
+    @Test
+    fun onEventTest() {
+        val mockDocumentSnapshot1 = getMockDocumentSnapshot(poi1.uid, poi1)
+        val mockDocumentSnapshot2 = getMockDocumentSnapshot(poi2.uid, poi2)
+
+        val documents = ArrayList<DocumentSnapshot>()
+        documents.add(mockDocumentSnapshot1)
+        documents.add(mockDocumentSnapshot2)
+
+        val expectedElements = HashMap<String, PointOfInterest>()
+        expectedElements[poi1.uid] = poi1
+        expectedElements[poi2.uid] = poi2
+
+        val mockSnapshot = Mockito.mock(QuerySnapshot::class.java)
+        Mockito.`when`(mockSnapshot.documents).thenReturn(documents)
+
+        db.onEvent(mockSnapshot, null)
+        assertEquals(db.elements, expectedElements)
+    }*/
 }
