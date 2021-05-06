0e8e9c9a98c7a60ce6501f2d2704bff5d35b68ca
Ewan Golfier <ewan.golfier@epfl.ch>
2021-05-06 16:17:24 +0200

    Test comments feature

--- app/src/androidTest/java/com/github/epfl/meili/forum/PostActivityTest.kt
+++ app/src/androidTest/java/com/github/epfl/meili/forum/PostActivityTest.kt
@@ -3,11 +3,15 @@ package com.github.epfl.meili.forum
 
 import android.content.Intent
 import android.net.Uri
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
@@ -26,7 +30,12 @@ import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.StorageTask
 import com.google.firebase.storage.UploadTask
+import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -45,6 +54,7 @@ class PostActivityTest {
         private const val TEST_POI_KEY = "POI_KEY"
         private const val TEST_ID = "ID"
         private val TEST_POST = Post("AUTHOR", "TITLE", "TEXT")
+        private val TEST_COMMENT = Comment("AUTHOR_COMMENT", "TEXT_COMMENT")
     }
 
     private val mockFirestore: FirebaseFirestore = Mockito.mock(FirebaseFirestore::class.java)
@@ -102,8 +112,8 @@ class PostActivityTest {
 
         val mockDocumentSnapshot: DocumentSnapshot = Mockito.mock(DocumentSnapshot::class.java)
         Mockito.`when`(mockDocumentSnapshot.id).thenReturn(TEST_ID)
-        Mockito.`when`(mockDocumentSnapshot.toObject(Post::class.java))
-            .thenReturn(TEST_POST)
+        Mockito.`when`(mockDocumentSnapshot.toObject(Comment::class.java))
+            .thenReturn(TEST_COMMENT)
         Mockito.`when`(mockSnapshotAfterAddition.documents).thenReturn(listOf(mockDocumentSnapshot))
 
         mockAuthenticationService.setMockUid(TEST_ID)
@@ -135,8 +145,99 @@ class PostActivityTest {
 
     @Test
     fun checkPostShown() {
+        commentsDatabase.onEvent(mockSnapshotBeforeAddition, null)
+
         onView(withId(R.id.post_author)).check(matches(withText(containsString(TEST_POST.author))))
         onView(withId(R.id.post_title)).check(matches(withText(containsString(TEST_POST.title))))
         onView(withId(R.id.post_text)).check(matches(withText(containsString(TEST_POST.text))))
+        onView(withId(R.id.comments_recycler_view)).check(matches(isEnabled()))
+    }
+
+    @Test
+    fun signedOutDisplayCheck() {
+        mockAuthenticationService.signOut()
+        commentsDatabase.onEvent(mockSnapshotBeforeAddition, null)
+
+        onView(withId(R.id.post_author)).check(matches(withText(containsString(TEST_POST.author))))
+        onView(withId(R.id.post_title)).check(matches(withText(containsString(TEST_POST.title))))
+        onView(withId(R.id.post_text)).check(matches(withText(containsString(TEST_POST.text))))
+
+        onView(withId(R.id.add_comment)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.edit_comment)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.comment_button)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.comments_recycler_view)).check(matches(isEnabled()))
+    }
+
+    @Test
+    fun signedInDisplayCheck() {
+        mockAuthenticationService.signInIntent()
+        commentsDatabase.onEvent(mockSnapshotBeforeAddition, null)
+
+        onView(withId(R.id.post_author)).check(matches(withText(containsString(TEST_POST.author))))
+        onView(withId(R.id.post_title)).check(matches(withText(containsString(TEST_POST.title))))
+        onView(withId(R.id.post_text)).check(matches(withText(containsString(TEST_POST.text))))
+
+        onView(withId(R.id.add_comment)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.edit_comment)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.comment_button)).check(matches(isDisplayed()))
+        onView(withId(R.id.comments_recycler_view)).check(matches(isEnabled()))
+    }
+
+    @Test
+    fun addCommentDisplayCheck() {
+        mockAuthenticationService.signInIntent()
+        commentsDatabase.onEvent(mockSnapshotBeforeAddition, null)
+
+        onView(withId(R.id.comment_button)).perform(click())
+
+        onView(withId(R.id.add_comment)).check(matches(isDisplayed()))
+        onView(withId(R.id.edit_comment)).check(matches(isDisplayed()))
+        onView(withId(R.id.comment_button)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.comments_recycler_view)).check(matches(isEnabled()))
+    }
+
+    @Test
+    fun addCommentTest() {
+        mockAuthenticationService.signInIntent()
+        commentsDatabase.onEvent(mockSnapshotBeforeAddition, null)
+
+        onView(withId(R.id.comment_button)).perform(click())
+        onView(withId(R.id.edit_comment)).perform(typeText(TEST_COMMENT.text), closeSoftKeyboard())
+        onView(withId(R.id.add_comment)).perform(click())
+
+        // send comments map with added comment to the database
+        commentsDatabase.onEvent(mockSnapshotAfterAddition, null)
+
+        onView(withId(R.id.add_comment)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.edit_comment)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.comment_button)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.comments_recycler_view))
+            .check(matches(isDisplayed()))
+            .perform(
+                RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(hasDescendant(withText(
+                    TEST_COMMENT.text))))
+
+        onView(textViewContainsText(TEST_COMMENT.text)).check(matches(isDisplayed()))
+        onView(textViewContainsText(TEST_COMMENT.author)).check(matches(isDisplayed()))
+
+        // Check edit text clears after having posted comment
+        onView(withId(R.id.comment_button)).perform(click())
+        onView(withId(R.id.edit_comment)).check(matches(withText("")))
+    }
+
+    private fun textViewContainsText(content: String): Matcher<View?> {
+        return object : TypeSafeMatcher<View?>() {
+            override fun describeTo(description: Description) {
+                description.appendText("A TextView with the text: $content")
+            }
+
+            override fun matchesSafely(item: View?): Boolean {
+                when (item) {
+                    is TextView -> return item.text == content
+                }
+                return false
+            }
+        }
     }
 }
