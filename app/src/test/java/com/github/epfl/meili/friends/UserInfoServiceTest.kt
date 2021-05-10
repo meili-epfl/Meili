5c1b3559b61cc2ae594af242b3297ca9d7ca2601
Marcel Torne <marcel.tornevillasevil@epfl.ch>
2021-05-10 16:36:57 +0200

    Add UserInfoService Test

--- app/src/test/java/com/github/epfl/meili/friends/UserInfoServiceTest.kt
+++ app/src/test/java/com/github/epfl/meili/friends/UserInfoServiceTest.kt
@@ -1,16 +1,20 @@
 package com.github.epfl.meili.friends
 
 import com.github.epfl.meili.database.FirestoreDocumentService
+import com.google.android.gms.gcm.Task
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
 import org.junit.Test
 import org.mockito.ArgumentMatcher
 import org.mockito.ArgumentMatchers
 import org.mockito.Mockito
 
 class UserInfoServiceTest {
-    private val TEST_UID
+    private val TEST_UID = "TEST_ID"
     @Test
     fun getUserInformationTest(){
-        val mockDocumentService = Mockito.mock(FirestoreDocumentService::class.java)
-        Mockito.`when`(mockDocumentService.getDocument(ArgumentMatchers.anyString()))
+        /*val mockDocument = Mockito.mock(DocumentSnapshot::class.java)
+        val mockDocumentService = Mockito.mock(FirestoreDocumentService::class.java)*/
+        //TODO
     }
 }
