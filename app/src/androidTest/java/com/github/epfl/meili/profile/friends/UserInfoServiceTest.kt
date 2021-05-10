fcf6c16063ad296b07e94f7ecf79a0617825abf2
Marcel Torne <marcel.tornevillasevil@epfl.ch>
2021-05-10 19:19:57 +0200

    Fix tests for user info service

--- app/src/androidTest/java/com/github/epfl/meili/profile/friends/UserInfoServiceTest.kt
+++ app/src/androidTest/java/com/github/epfl/meili/profile/friends/UserInfoServiceTest.kt
@@ -1,10 +1,12 @@
-package com.github.epfl.meili.friends
+package com.github.epfl.meili.profile.friends
 
 import com.github.epfl.meili.database.FirestoreDocumentService
 import com.github.epfl.meili.models.User
-import com.github.epfl.meili.profile.friends.UserInfoService
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.mockito.Mockito
@@ -22,20 +24,25 @@ class UserInfoServiceTest {
         Mockito.`when`(mockDocument.toObject(User::class.java)).thenReturn(testUser)
         val mockTask = Mockito.mock(Task::class.java)
         Mockito.`when`(mockTask.addOnSuccessListener(Mockito.any())).then {
-            val listener = it.arguments[0] as (DocumentSnapshot) -> Unit
-            listener(mockDocument)
+            val listener = it.arguments[0] as OnSuccessListener<DocumentSnapshot>
+            listener.onSuccess(mockDocument)
             return@then null
         }
-        val mockDocumentService = Mockito.mock(FirestoreDocumentService::class.java)
-        Mockito.`when`(mockDocumentService.getDocument(Mockito.anyString())).thenReturn(mockTask as Task<DocumentSnapshot>)
+
+        val mockDocumentReference = Mockito.mock(DocumentReference::class.java)
+        Mockito.`when`(mockDocumentReference.get()).thenReturn(mockTask as Task<DocumentSnapshot>)
+        val mockFirestore = Mockito.mock(FirebaseFirestore::class.java)
+        Mockito.`when`(mockFirestore.document(Mockito.anyString())).thenReturn(mockDocumentReference)
+
+        FirestoreDocumentService.databaseProvider = { mockFirestore }
 
         val testList = ArrayList<String>()
         testList.add(TEST_UID)
         val expectedResult = HashMap<String, User>()
         expectedResult[TEST_UID] = testUser
 
-        val onSuccess = { it:Map<String, User> -> assertEquals(it, expectedResult)}
+        val onSuccess = { it: Map<String, User> -> assertEquals(it, expectedResult) }
 
-        UserInfoService().getUserInformation(testList, onSuccess ,{assert(false)})
+        UserInfoService().getUserInformation(testList, onSuccess, { assert(false) })
     }
 }
