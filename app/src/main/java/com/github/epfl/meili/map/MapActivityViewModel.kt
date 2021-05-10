ce4615e529f37667a4f9b25df08b91ce6c9a8891
Aman Bansal <amanbansal3014@protonmail.com>
2021-05-10 16:12:42 +0200

    fix azimuth tolerance calc

--- app/src/main/java/com/github/epfl/meili/map/MapActivityViewModel.kt
+++ app/src/main/java/com/github/epfl/meili/map/MapActivityViewModel.kt
@@ -17,15 +17,14 @@ import com.google.firebase.ml.vision.cloud.landmark.FirebaseVisionCloudLandmark
 import com.google.maps.android.SphericalUtil.computeDistanceBetween
 import com.google.maps.android.SphericalUtil.computeHeading
 import kotlin.math.PI
-import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
 
 class MapActivityViewModel(application: Application): PoiMarkerViewModel(application) {
     companion object {
-        private const val SENSOR_DELAY = 2000_000 // microseconds
-        private const val FIELD_OF_VIEW = 40 // degrees
-        private const val LENS_MAX_DISTANCE: Double = 500.0 // meters
-        private const val AZIMUTH_TOLERANCE: Double = 5.0 // degrees
+        private const val SENSOR_DELAY = 500_000 // microseconds
+        private const val FIELD_OF_VIEW = 40.0 // degrees
+        private const val LENS_MAX_DISTANCE = 500.0 // meters
+        private const val AZIMUTH_TOLERANCE = 5.0 // degrees
     }
 
     private var sensorManager: SensorManager =
@@ -55,12 +54,11 @@ class MapActivityViewModel(application: Application): PoiMarkerViewModel(applica
         override fun onSensorChanged(event: SensorEvent) {
             floatGeoMagnetic = event.values
             updateOrientation()
-            // FIXME
-            if ((lastAzimuth - azimuth()).absoluteValue > AZIMUTH_TOLERANCE) {
+            if (!checkAnglesClose(azimuthInDegrees(), lastAzimuth, AZIMUTH_TOLERANCE)) {
                 mPOIDist.value = closestPoiAndDistance(fieldOfViewPOIs())
             }
 
-            lastAzimuth = azimuth()
+            lastAzimuth = azimuthInDegrees()
         }
 
         override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
@@ -93,7 +91,11 @@ class MapActivityViewModel(application: Application): PoiMarkerViewModel(applica
         SensorManager.getOrientation(floatRotationMatrix, floatOrientation)
     }
 
-    private fun azimuth(): Double = floatOrientation[0] * 180 / PI
+    private fun azimuthInDegrees(): Double = floatOrientation[0] * 180 / PI
+
+    private fun checkAnglesClose(a: Double, b: Double, tolerance: Double): Boolean {
+        return (a - tolerance / 2).rem(360) <= b && b <= (a + tolerance / 2).rem(360)
+    }
 
     // assumes lastUserLocation not null
     private fun getUserLocation() = LatLng(lastUserLocation!!.latitude, lastUserLocation!!.longitude)
@@ -124,15 +126,12 @@ class MapActivityViewModel(application: Application): PoiMarkerViewModel(applica
         val pois = ArrayList<PointOfInterest>()
 
         val userLocation = getUserLocation()
-        val azimuth = azimuth() + 180
+        val azimuth = azimuthInDegrees() + 180
 
         for (poi in mPointsOfInterest.value!!.values) {
             val poiLocation = LatLng(poi.latitude, poi.longitude)
             val angle = computeHeading(userLocation, poiLocation) + 180
-            if (
-                (azimuth - FIELD_OF_VIEW / 2).rem(360) <= angle &&
-                angle <= (azimuth + FIELD_OF_VIEW / 2).rem(360)
-            ) {
+            if (checkAnglesClose(azimuth, angle, FIELD_OF_VIEW)) {
                 pois.add(poi)
             }
         }
