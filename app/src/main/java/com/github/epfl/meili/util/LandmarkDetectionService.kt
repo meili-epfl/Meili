ddfeaad2107ace5b5b4a164fe342094e1f550a11
Aman Bansal <amanbansal3014@protonmail.com>
2021-05-11 15:14:56 +0200

    add landmark service documentation

--- app/src/main/java/com/github/epfl/meili/util/LandmarkDetectionService.kt
+++ app/src/main/java/com/github/epfl/meili/util/LandmarkDetectionService.kt
@@ -8,11 +8,17 @@ import com.google.firebase.ml.vision.cloud.landmark.FirebaseVisionCloudLandmark
 import com.google.firebase.ml.vision.cloud.landmark.FirebaseVisionCloudLandmarkDetector
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 
+/**
+ * Uses FirebaseVision to detect landmarks in an image
+ */
 object LandmarkDetectionService {
-    var firebaseVision:() -> FirebaseVision = { FirebaseVision.getInstance() }
+    var firebaseVision: () -> FirebaseVision = { FirebaseVision.getInstance() }
     var firebaseVisionImage: (context: Context, imageUri: Uri) -> FirebaseVisionImage
         = { context, imageUri -> FirebaseVisionImage.fromFilePath(context, imageUri) }
 
+    /**
+     * Returns the FirebaseVision landmark detection task given an image uri and the application context
+     */
     fun detectInImage(context: Context, imageUri: Uri): Task<List<FirebaseVisionCloudLandmark>> =
         firebaseVision().visionCloudLandmarkDetector.detectInImage(firebaseVisionImage(context, imageUri))
 }
