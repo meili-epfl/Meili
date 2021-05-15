01c2f0efcd4b96115c9dadd9afdfbf9af148ec36
Ahmed Ezzo <ahmed.ezzo@epfl.ch>
2021-05-15 13:32:09 +0200

    readded fcm key

--- app/src/main/java/com/github/epfl/meili/models/NotificationData.kt
+++ app/src/main/java/com/github/epfl/meili/models/NotificationData.kt
@@ -13,7 +13,7 @@ data class NotificationData(
         const val BASE_URL = "https://fcm.googleapis.com"
 
         //Meili FCM server key
-        const val SERVER_KEY = ""
+        const val SERVER_KEY = "AAAAQMGOSZE:APA91bGNQOI6HlXae8xBeMAB30q2vsNZoNzsPpcxfO2ytTiJl4HLdiw6oQDtB6KlIdu06Qu5QSLsg3tAIgHOT4tTfErPG_cyrF1d9ULGwrOmY7swzgJ--bzjsJi1Iq9fUVY9M9zilggV"
         //used json conversion type
         const val CONTENT_TYPE = "application/json"
     }
