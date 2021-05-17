4c521852a3ca6d21d44a7531ee0909471b195d78
Ewan Golfier <ewan.golfier@epfl.ch>
2021-05-17 16:46:09 +0200

    Create files files

---
+++ app/src/main/java/com/github/epfl/meili/profile/myposts/MyPostsActivity.kt
@@ -0,0 +1,12 @@
+package com.github.epfl.meili.profile.myposts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.epfl.meili.R
+
+class MyPostsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_posts)
+    }
+}
