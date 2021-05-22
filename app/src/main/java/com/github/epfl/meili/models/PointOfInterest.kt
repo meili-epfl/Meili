a34613c9532c4535ab77be412a78a8e38dc63f9f
Aman Bansal <amanbansal3014@protonmail.com>
2021-05-22 14:17:24 +0200

    use built in java hashcode

--- app/src/main/java/com/github/epfl/meili/models/PointOfInterest.kt
+++ app/src/main/java/com/github/epfl/meili/models/PointOfInterest.kt
@@ -3,6 +3,8 @@ package com.github.epfl.meili.models
 import android.os.Parcelable
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.parcelize.Parcelize
+import java.util.*
+import kotlin.collections.ArrayList
 
 @Parcelize
 data class PointOfInterest(
@@ -14,6 +16,13 @@ data class PointOfInterest(
     var poiTypes: List<String> = ArrayList(),
     var openNow: Boolean? = null
 ) : Parcelable {
+
+    companion object {
+        const val START_CHAR = '{'
+        const val END_CHAR = '}'
+        const val TAG = "PointOfInterest"
+    }
+
     override fun toString(): String {
         return START_CHAR + "POI:" + "lat:" + latitude + "long:" + longitude + ",name:" + name + ",uid:" + uid + ",icon:" + icon + END_CHAR
     }
@@ -32,20 +41,5 @@ data class PointOfInterest(
         return LatLng(latitude, longitude)
     }
 
-    override fun hashCode(): Int {
-        var result = latitude.hashCode()
-        result = 31 * result + longitude.hashCode()
-        result = 31 * result + name.hashCode()
-        result = 31 * result + uid.hashCode()
-        result = 31 * result + icon.hashCode()
-        result = 31 * result + poiTypes.hashCode()
-        result = 31 * result + (openNow?.hashCode() ?: 0)
-        return result
-    }
-
-    companion object {
-        const val START_CHAR = '{'
-        const val END_CHAR = '}'
-        const val TAG = "PointOfInterest"
-    }
+    override fun hashCode(): Int = Objects.hashCode(this)
 }
