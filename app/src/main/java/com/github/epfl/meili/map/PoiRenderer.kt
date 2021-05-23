c9a85e53c41b647caea4a3c63b2f2f511445ac6e
Marcel Torne <marcel.tornevillasevil@epfl.ch>
2021-05-23 09:57:37 +0200

    Set up poi renderer to accept meili lens analyzed poi

--- app/src/main/java/com/github/epfl/meili/map/PoiRenderer.kt
+++ app/src/main/java/com/github/epfl/meili/map/PoiRenderer.kt
@@ -11,17 +11,19 @@ import com.google.maps.android.clustering.view.DefaultClusterRenderer
 /**
  * PoiRenderer is the class which renders POIs as Markers on the Map and also handles cluster rendering
  */
-open class PoiRenderer(context: Context?, map: GoogleMap?, val clusterManager: ClusterManager<PoiItem>)
+open class PoiRenderer(context: Context?, map: GoogleMap?, private val clusterManager: ClusterManager<PoiItem>)
     : DefaultClusterRenderer<PoiItem>(context, map, clusterManager) {
 
     private var poiStatusMap: Map<PoiItem, PointOfInterestStatus>? = null
+    private var meiliLensPoi: PoiItem? = null
 
     override fun onBeforeClusterItemRendered(item: PoiItem, markerOptions: MarkerOptions) {
-        val icon: BitmapDescriptor
-        if (poiStatusMap == null || !poiStatusMap!!.contains(item)) {
-            icon = DEFAULT_ICON
+        val icon: BitmapDescriptor = if (meiliLensPoi == item) {
+            MEILI_LENS_ICON
+        } else if (poiStatusMap == null || !poiStatusMap!!.contains(item)) {
+            DEFAULT_ICON
         } else {
-            icon = when (poiStatusMap!![item]) {
+            when (poiStatusMap!![item]) {
                 PointOfInterestStatus.REACHABLE -> REACHABLE_ICON
                 PointOfInterestStatus.VISITED -> VISITED_ICON
                 PointOfInterestStatus.VISIBLE -> VISIBLE_ICON
@@ -42,9 +44,10 @@ open class PoiRenderer(context: Context?, map: GoogleMap?, val clusterManager: C
     }
 
     companion object {
-        val REACHABLE_ICON = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)
-        val VISITED_ICON = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)
-        val VISIBLE_ICON = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)
-        val DEFAULT_ICON = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
+        val REACHABLE_ICON: BitmapDescriptor = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)
+        val VISITED_ICON: BitmapDescriptor = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)
+        val VISIBLE_ICON: BitmapDescriptor = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)
+        val DEFAULT_ICON: BitmapDescriptor = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
+        val MEILI_LENS_ICON: BitmapDescriptor = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)
     }
 }
