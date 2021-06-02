04fe88412de05dbe2a8b76e856b6f0ac8dd7daac
Ewan Golfier <ewan.golfier@epfl.ch>
2021-06-02 15:44:31 +0200

    Add Cropper assertions

--- app/src/androidTest/java/com/github/epfl/meili/photo/PhotoCropActivityTest.kt
+++ app/src/androidTest/java/com/github/epfl/meili/photo/PhotoCropActivityTest.kt
@@ -1,10 +1,15 @@
 package com.github.epfl.meili.photo
 
+import android.app.Activity
+import android.widget.ImageView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.rule.GrantPermissionRule
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
+import androidx.test.runner.lifecycle.Stage
 import com.github.epfl.meili.R
 import org.junit.Before
 import org.junit.Rule
@@ -15,7 +20,8 @@ import org.junit.runner.RunWith
 class PhotoCropActivityTest {
 
     @get:Rule
-    var testRule: ActivityScenarioRule<CameraActivity> = ActivityScenarioRule(CameraActivity::class.java)
+    var testRule: ActivityScenarioRule<CameraActivity> =
+        ActivityScenarioRule(CameraActivity::class.java)
 
     @get:Rule
     var mGrantPermissionRule: GrantPermissionRule =
@@ -29,23 +35,76 @@ class PhotoCropActivityTest {
         onView(withId(R.id.camera_capture_button)).perform(click())
         Thread.sleep(1500)
     }
-
-    //TODO: no assertions in any of the below tests, we can add some view assertions or something?
+    
     @Test
     fun rotate90Click() {
+        var currentActivity: Activity? = null
+        getInstrumentation().runOnMainSync {
+            run {
+                currentActivity =
+                    ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(
+                        Stage.RESUMED
+                    ).elementAtOrNull(0)
+            }
+        }
+
+        val photoView = currentActivity!!.findViewById<ImageView>(R.id.photo_edit_image_view)
+        val drawable = photoView.drawable
+
+        // Check rotates
         onView(withId(R.id.rotate_90)).perform(click())
+        assert(photoView.rotation == 90f)
+        onView(withId(R.id.rotate_90)).perform(click())
+        assert(photoView.rotation == 180f)
+
+        // Check the image hasn't changed
+        assert(photoView.drawable == drawable)
     }
 
     @Test
     fun cropModeButtonClick() {
+        var currentActivity: Activity? = null
+        getInstrumentation().runOnMainSync {
+            run {
+                currentActivity =
+                    ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(
+                        Stage.RESUMED
+                    ).elementAtOrNull(0)
+            }
+        }
+
+        val photoView = currentActivity!!.findViewById<ImageView>(R.id.photo_edit_image_view)
+        val drawable = photoView.drawable
+
         onView(withId(R.id.crop_mode_button)).perform(click())
         onView(withId(R.id.crop_mode_button)).perform(click())
+
+        // Check the image hasn't changed
+        assert(photoView.drawable == drawable)
     }
 
     @Test
     fun cropClick() {
+        var currentActivity: Activity? = null
+        getInstrumentation().runOnMainSync {
+            run {
+                currentActivity =
+                    ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(
+                        Stage.RESUMED
+                    ).elementAtOrNull(0)
+            }
+        }
+
+        val photoView = currentActivity!!.findViewById<ImageView>(R.id.photo_edit_image_view)
+        val drawable = photoView.drawable
+
         onView(withId(R.id.crop_mode_button)).perform(click())
         onView(withId(R.id.crop)).perform(click())
+
+        // Check the image has changed
+        assert(photoView.drawable != drawable)
+        assert(photoView.drawable.intrinsicWidth < drawable.intrinsicWidth
+                || photoView.drawable.intrinsicHeight < drawable.intrinsicHeight)
     }
 
     @Test
