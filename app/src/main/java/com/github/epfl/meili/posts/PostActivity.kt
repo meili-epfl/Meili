52e55be6cfc7dc19da8494330ec1ffc967def967
Marcel Torne <marcel.tornevillasevil@epfl.ch>
2021-06-04 09:20:26 +0200

    Refactor on click user name

--- app/src/main/java/com/github/epfl/meili/posts/PostActivity.kt
+++ app/src/main/java/com/github/epfl/meili/posts/PostActivity.kt
@@ -58,13 +58,6 @@ class PostActivity : AppCompatActivity(), ClickListener, ListSorter<Comment> {
         initRecyclerAdapter()
         initLoggedInListener()
         initSorting(findViewById(R.id.sort_spinner), R.array.sort_array_comments)
-
-        findViewById<TextView>(R.id.userName).setOnClickListener {
-            openUserProfile(post.authorUid)
-        }
-        findViewById<CircleImageView>(R.id.userImage).setOnClickListener {
-            openUserProfile(post.authorUid)
-        }
     }
 
     private fun initViews(post: Post) {
